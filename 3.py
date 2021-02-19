ea0e78dd7007843bfd62630ec851a01587b38da1
semenovoleg988 <36897524+semenovoleg988@users.noreply.github.com>
2021-02-19 15:44:57 +0200

    fixed orthografy and loop for all even without three

--- 3.py
+++ 3.py
@@ -10,21 +10,21 @@ def largest_prime_factor(number: int) -> int:
     """ Find the largest prime divisor of given number."""
     divisor = 2
     max_prime_factor = 1
-    if is_devisor(number, divisor):#divide by 2 if possible
+    if is_divisor(number, divisor):#divide by 2 if possible
         number /= divisor
-        if is_devisor(number, divisor):
+        if is_divisor(number, divisor):
             number /= divisor
-            while is_devisor(number, divisor):
+            while is_divisor(number, divisor):
                 number /= divisor
             divisor += 1
         else:
             max_prime_factor = divisor; divisor += 1
     else: divisor += 1
-    if is_devisor(number, divisor):#divide by 3 if possible
+    if is_divisor(number, divisor):#divide by 3 if possible
         number /= divisor
-        if is_devisor(number, divisor):
+        if is_divisor(number, divisor):
             number /= divisor
-            while is_devisor(number, divisor):
+            while is_divisor(number, divisor):
                 number /= divisor
             divisor += 2
         else:
@@ -32,20 +32,20 @@ def largest_prime_factor(number: int) -> int:
     else: divisor += 2
 
     while divisor <= number:#for all even without 3
-        if is_devisor(number, divisor):
+        if is_divisor(number, divisor):
             number /= divisor
-            if is_devisor(number, divisor):
+            if is_divisor(number, divisor):
                 number /= divisor
-                while is_devisor(number, divisor):
+                while is_divisor(number, divisor):
                     number /= divisor
             else:
                 max_prime_factor = divisor
-        elif is_devisor(number, divisor + 2):
+        if is_divisor(number, divisor + 2):
             divisor_2 = divisor + 2
             number /= divisor_2
-            if is_devisor(number, divisor_2):
+            if is_divisor(number, divisor_2):
                 number /= divisor_2
-                while is_devisor(number, divisor_2):
+                while is_divisor(number, divisor_2):
                     number /= divisor_2
             else:
                 max_prime_factor = divisor_2
@@ -53,8 +53,8 @@ def largest_prime_factor(number: int) -> int:
     return max_prime_factor
 
 
-def is_devisor(number: int, divisor: int) -> bool:
-    """ Define is devisor or not."""
+def is_divisor(number: int, divisor: int) -> bool:
+    """ Define is divisor or not."""
     if number == 0 or divisor > number:
         return False
     return number % divisor == 0
