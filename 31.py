94852d36a131c8e2d59aa84b8ef3dcd2afe9e911
semenovoleg988 <36897524+semenovoleg988@users.noreply.github.com>
2021-02-27 19:11:19 +0200

    done

--- 31.py
+++ 31.py
@@ -3,10 +3,18 @@
 # Description(https://projecteuler.net/problem=31)
 
 
-def coin_sums() -> None:
-    """ """
-    pass
+def pound_and_pences_combinations(pounds: int, pences: int, coins = [1, 2, 5, 10, 20, 50, 100, 200]):
+    """ Calculate number of combinations of pounds and pences."""
+    sum = pounds * 100 + pences
+    number_of_results = 0
+    if sum == 0:
+        return 1
+    if sum < 0 or coins == []:
+        return number_of_results
+    for i in range(sum // coins[-1] + 1):
+        number_of_results += pound_and_pences_combinations(0, sum - i*coins[-1], coins = coins[:-1])
+    return number_of_results
 
 
 if __name__ == "__main__":
-    pass
+    print(pound_and_pences_combinations(2, 0))
