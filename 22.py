d84535b3625b88154898e1e7b3e04799f9faabd7
semenovoleg988 <semenovoleg988@gmail.com>
2023-03-12 22:27:44 +0200

    approved by projecteuler.net

--- 22.py
+++ 22.py
@@ -2,7 +2,7 @@
 # Names scores
 """ Description(https://projecteuler.net/problem=22).
 
-    Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
+    Using names.txt, a 46K text file containing over five-thousand first names, 
     begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this 
     value by its alphabetical position in the list to obtain a name score.
 
@@ -14,10 +14,25 @@
 
 def __main__():
     l = []
+    total = 0
+
     with open("22.txt") as f:
         for line in f:
-            l.append(line.split(sep=","))
-    print(l)
+            l.extend(line.split(sep=","))
+    
+    for i in range(len(l)):
+        l[i] = l[i].strip("\"")
+    
+
+    l.sort()
+    for i in range(len(l)):
+        score = 0
+        for char in l[i]:
+            score += ord(char) - 64
+        score *= (i+1)
+        total += score
+    print(total)
+
 
 if __name__ == "__main__":
     __main__()
